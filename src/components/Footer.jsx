645e06f83a11213adf6776edf7e707fad4f92534
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2021-10-17 23:13:47 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = (props) => {
 
     return <div className={`footer-container ${className}`}>
         <div className="wrap-icon">
-            <a href="https://www.facebook.com/DoHungAnh27092000/"><i className="fab fa-facebook h3"></i></a>
-            <a href="https://github.com/DoHungAnh-27-09-2000"><i className="fab fa-github ms-4 text-dark h3"></i></a>
+            <a href="https://www.facebook.com/luong.huyen.9822"><i className="fab fa-facebook h3"></i></a>
+            <a href="https://github.com/DoHuyenLuong"><i className="fab fa-github ms-4 text-dark h3"></i></a>
             <a href="https://www.youtube.com/channel/UCuZUp0A9aLrfHSPr3eg05Ow"><i className="fab fa-youtube ms-4 text-danger h3"></i></a>
-            <a href="mailto:dohunganh27092000@gmail.com"><i className="far fa-envelope ms-4 text-danger h3"></i></a>
+            <a href="mailto:huyenluong80832000@gmail.com"><i className="far fa-envelope ms-4 text-danger h3"></i></a>
         </div>
         <p style={{ fontWeight: 'bold', textAlign: 'center', margin: '0' }}>
             ©{new Date().getFullYear()}. ALL RIGHTS RESERVED. | Design by
