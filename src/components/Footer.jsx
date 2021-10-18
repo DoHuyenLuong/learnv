827e164bdd3f33455c7bc3890b68db3fb68e1c99
Đỗ Huyền Lương <84034399+DoHuyenLuong@users.noreply.github.com>
2021-10-18 18:26:24 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = (props) => {
         <div className="wrap-icon">
             <a href="https://www.facebook.com/Learn-Vocabulary-109966034797404"><i className="fab fa-facebook h3"></i></a>
             <a href="https://github.com/DoHuyenLuong"><i className="fab fa-github ms-4 text-dark h3"></i></a>
-            <a href="https://www.youtube.com/channel/UCuZUp0A9aLrfHSPr3eg05Ow"><i className="fab fa-youtube ms-4 text-danger h3"></i></a>
+            <a href="https://www.youtube.com/channel/UCOAVNCWLVYtKEvu_omXYkoQ/featured"><i className="fab fa-youtube ms-4 text-danger h3"></i></a>
             <a href="mailto:huyenluong80832000@gmail.com"><i className="far fa-envelope ms-4 text-danger h3"></i></a>
         </div>
         <p style={{ fontWeight: 'bold', textAlign: 'center', margin: '0' }}>
